f9ba8b40dc8fdfde68ea88b8012e54fb9b1c086b
Fernando <Fernando@172.21.151.238>
2017-10-05 21:56:05 -0300

    Criação de função com retorno. Criação de um pacote com uma classe de exemplo.

--- src/algoritmos/Algoritmos.java
+++ src/algoritmos/Algoritmos.java
@@ -5,13 +5,42 @@ package algoritmos;
 public class Algoritmos {
 
 
+    
+    
+    public static void quadrado(double n){
+    
+    double r = n * n;
+ 
+    }
+    
+    public static double metade(double v){
+    
+        double r = v / 2;
+    
+   return r;
+    
+    }
+    
+    
+    
     public static void main(String[] args) {
    
+        double raiz;
+        raiz = Math.sqrt(15);
+        
+        System.out.println("A raiz é " + Math.sqrt(15) );
+        //System.out.println("A raiz é " + quadrado(15) );
+         quadrado(15) ;
+        System.out.println("A metade é " + metade(15) );
         
         
         
         
+        quadrado(15);
         
+        double resul;
+        resul = metade(15);
+        System.out.println();
         
     }
     
